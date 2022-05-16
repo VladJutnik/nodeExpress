799c2d5ad1d4216e669bad46395b7e75beee04a8
VladJutnik <v2517v@gmail.com>
2022-05-16 16:36:24 +0700

    Layouts And Partials

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const express = require('express')
+const express = require('express'),
+    path = require('path'),
+    exhds = require('express-handlebars')
+const app = express()
 
+const hbs = exhds.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+app.engine('hbs', hbs.engine)//регестрируем что используем хандербарс
+app.set('view engine', 'hbs')//тут мы уже говорим что начали его использовать
+app.set('views', 'views')
+app.get('/', (req, res,next)=>{
+    res.render('index')
+})
+app.get('/about', (req, res,next)=>{
+    res.render('about')
+})
+/*
+//Тут простой пример как работать с html страницами, однако они не денамические мы не можем что то перадать туда хз кнч
+app.get('/', (req, res,next)=>{
+    res.sendFile(path.join(__dirname, 'views','index.html'))
+})
+app.get('/about', (req, res,next)=>{
+    res.sendFile(path.join(__dirname, 'views','about.html'))
+})*/
 const PORT = process.env.PORT || 3002
-//
-const app = express(
-
-)
 app.listen(PORT, ()=>{
     console.log(`Сервер запущен ${PORT}`)
 })
